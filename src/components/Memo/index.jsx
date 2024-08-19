43a42abde82a1f2cae4dd71ab09a23f097a2ff01
Zourunfa <473023989@qq.com>
2024-08-19 22:29:46 +0800

    feat: memo

--- src/components/Memo/index.jsx
+++ src/components/Memo/index.jsx
@@ -14,7 +14,15 @@ const ChildComponent =memo( (props)=>{
   useEffect(()=>{
     console.log('ChildComponent render')
   })
-  return <div><p>{props.count2}</p></div>
+  /**
+   * memo核心会对引用进行比较，这种比较是浅的比较。
+   * 只会看引用是不是新的，ChildData引用，如果更新一个新的
+   * 引用，那么child就会执行
+   * 
+   * 
+   * count1更新, UseMemo必然会执行，childData必然会赋值一个新的引用
+   */
+  return <div><p>{props.childData.count2}</p></div>
 })
 
 const UseMemo = () =>{
@@ -22,13 +30,15 @@ const UseMemo = () =>{
     const [count2, setCount2] = useState(0);
 
 
-    
+    const childData = {
+      count2
+    }
     return (
       <div>
         <h2>{count1}</h2>
         <button onClick={() => setCount1(count1 + 1)}>Increment1</button>
       
-        <ChildComponent count2={count2} />
+        <ChildComponent childData ={childData} />
         <button onClick={() => setCount2(count2 + 1)}>Increment</button>
       </div>
     )
