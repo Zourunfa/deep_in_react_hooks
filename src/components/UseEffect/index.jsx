3ec6486cd34fe7211134691605c2aac6f478d3fd
Zourunfa <473023989@qq.com>
2024-08-25 22:43:36 +0800

    feat: 手写useEffect

--- src/components/UseEffect/index.jsx
+++ src/components/UseEffect/index.jsx
@@ -1,1 +1,13 @@
-// 手写useEffect
+let _deps; // _deps 记录 useEffect 上一次的 依赖
+
+function useEffect(callback, depArray) {
+  const hasNoDeps = !depArray; // 如果 dependencies 不存在
+  const hasChangedDeps = _deps
+    ? !depArray.every((el, i) => el === _deps[i]) // 两次的 dependencies 是否完全相等
+    : true;
+  /* 如果 dependencies 不存在，或者 dependencies 有变化*/
+  if (hasNoDeps || hasChangedDeps) {
+    callback();
+    _deps = depArray;
+  }
+}
